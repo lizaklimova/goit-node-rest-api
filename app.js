4c5366b5e94e638ed43dca69e5be1d9f03a7dbcb
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-01-23 13:24:28 +0200

    added mongoose

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import mongoose from "mongoose";
 import contactsRouter from "./routes/contactsRouter.js";
+
+const DB_HOST =
+  "mongodb+srv://liza_klymova:rZdGNcLnt2X2NcXm@cluster0.th9vtkn.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
 const app = express();
 
@@ -19,8 +35,4 @@ app.use((_, res) => {
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
 });
