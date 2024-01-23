7cbe4d2c1f4895435e7a0a1e3469c1719f9e8b34
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-01-23 22:36:20 +0200

    finished

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.use((err, _, res) => {
+app.use((err, _, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
