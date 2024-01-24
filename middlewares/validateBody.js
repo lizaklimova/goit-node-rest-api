2bb4a565dbb1fa2bf88ec5bf56193e1f61f9b47d
lizaklimova <132839549+lizaklimova@users.noreply.github.com>
2024-01-24 20:50:54 +0200

    finished

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -2,15 +2,10 @@ import HttpError from "../helpers/HttpError.js";
 
 const validateBody = (schema) => {
   const func = (req, _, next) => {
-    const fieldsQty = Object.keys(req.body).length;
     const { error } = schema.validate(req.body);
 
     if (error) {
       next(HttpError(400, error.message));
-    }
-
-    if (!fieldsQty) {
-      next(HttpError(400, "Body must have at least one field"));
     }
 
     next();
